af840739570c7bdf2cb6cd77d81c775f27714c31
Moosbee <bernhardr91104@gmail.com>
2024-04-03 11:25:59 +0200

    add api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { AgentsApi } from "./components/api/api";
+import { Configuration } from "./components/api/configuration";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function test() {
+    const openapiConfig = new Configuration();
+    openapiConfig.baseOptions = {
+      headers: {
+        Authorization:
+          "Bearer " + import.meta.env.VITE_SPACE_TRADERS_CLIENT_AGENT_TOKEN,
+      },
+    };
+    const openapi = new AgentsApi(openapiConfig);
+    setCount((count) => count + 1);
+
+    openapi
+      .getMyAgent()
+      .then((value) => {
+        console.log("value", value, value.data.data.symbol);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
 
   return (
     <>
@@ -18,9 +41,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => test()}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +50,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
