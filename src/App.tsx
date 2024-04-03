deaa8f95c95c90445e70ba88fd2dcdc891b27684
Moosbee <bernhardr91104@gmail.com>
2024-04-03 15:17:21 +0200

    some more setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { AgentsApi } from "./components/api/api";
-import { Configuration } from "./components/api/configuration";
+import spaceTraderClient from "./spaceTraderClient";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  function test() {
-    const openapiConfig = new Configuration();
-    openapiConfig.baseOptions = {
-      headers: {
-        Authorization:
-          "Bearer " + import.meta.env.VITE_SPACE_TRADERS_CLIENT_AGENT_TOKEN,
-      },
-    };
-    const openapi = new AgentsApi(openapiConfig);
+  async function test() {
     setCount((count) => count + 1);
-
-    openapi
-      .getMyAgent()
-      .then((value) => {
-        console.log("value", value, value.data.data.symbol);
-      })
-      .catch((err) => {
-        console.log("err", err);
-      });
+    console.log("test", await spaceTraderClient.AgentsClient.getMyAgent());
   }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => test()}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => test()}>count is {count}</button>
     </>
   );
 }
---
+++ src/spaceTraderClient.ts
@@ -0,0 +1,30 @@
+import {
+  FleetApi,
+  AgentsApi,
+  SystemsApi,
+  FactionsApi,
+  ContractsApi,
+} from "./components/api/api";
+import { Configuration } from "./components/api/configuration";
+
+const openapiConfig = new Configuration();
+openapiConfig.baseOptions = {
+  headers: {
+    Authorization:
+      "Bearer " + import.meta.env.VITE_SPACE_TRADERS_CLIENT_AGENT_TOKEN,
+  },
+};
+
+const FleetClient = new FleetApi(openapiConfig);
+const AgentsClient = new AgentsApi(openapiConfig);
+const SystemsClient = new SystemsApi(openapiConfig);
+const FactionsClient = new FactionsApi(openapiConfig);
+const ContractsClient = new ContractsApi(openapiConfig);
+
+export default {
+  FleetClient: FleetClient,
+  AgentsClient: AgentsClient,
+  SystemsClient: SystemsClient,
+  FactionsClient: FactionsClient,
+  ContractsClient: ContractsClient,
+};
