751b5cd3088d02ced6de7689ce02676e923e07e5
Moosbee <bernhardr91104@gmail.com>
2024-04-04 16:11:54 +0200

    add myAgent

---
+++ src/components/agentDisp/AgentDisp.tsx
@@ -0,0 +1,23 @@
+import { Agent } from "../api";
+import classes from "./AgentDisp.module.css";
+
+function AgentDisp({ agent }: { agent: Agent }) {
+  return (
+    <div className={classes.agent}>
+      <div>Symbol</div>
+      <div>{agent.symbol}</div>
+      <div>Account Id</div>
+      <div>{agent.accountId}</div>
+      <div>Credits</div>
+      <div>{agent.credits}</div>
+      <div>Ship count</div>
+      <div>{agent.shipCount}</div>
+      <div>Headquarters</div>
+      <div>{agent.headquarters}</div>
+      <div>Starting Faction</div>
+      <div>{agent.startingFaction}</div>
+    </div>
+  );
+}
+
+export default AgentDisp;
--- src/sites/agents/Agents.tsx
+++ src/sites/agents/Agents.tsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import { Agent } from "../../components/api";
+import spaceTraderClient from "../../spaceTraderClient";
 import classes from "./Agents.module.css";
+import AgentDisp from "../../components/agentDisp/AgentDisp";
 
 function Agents() {
-  return <div className={classes.agents}>Agents</div>;
+  const [myAgent, setMyAgent] = useState<Agent>({
+    credits: 0,
+    headquarters: "",
+    shipCount: 0,
+    startingFaction: "",
+    symbol: "",
+  });
+  useEffect(() => {
+    spaceTraderClient.AgentsClient.getMyAgent().then((response) => {
+      console.log("response", response);
+      setMyAgent(response.data.data);
+      response.data.data;
+    });
+    return () => {};
+  }, []);
+
+  return (
+    <div className={classes.agents}>
+      <AgentDisp agent={myAgent}></AgentDisp>
+    </div>
+  );
 }
 
 export default Agents;
