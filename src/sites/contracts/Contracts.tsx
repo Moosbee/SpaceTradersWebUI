1d816212b90b5712bad29738f7d67bca38631a42
Moosbee <bernhardr91104@gmail.com>
2024-06-04 11:27:16 +0200

    factions list & contracts + icon import

--- src/sites/contracts/Contracts.tsx
+++ src/sites/contracts/Contracts.tsx
@@ -1,5 +1,115 @@
+import { PaginationProps, Pagination, Flex, Card, Descriptions } from "antd";
+import { useState, useEffect } from "react";
+import { Contract } from "../../components/api";
+import spaceTraderClient from "../../spaceTraderClient";
+
 function Contracts() {
-  return <div>Contracts</div>;
+  const [contracts, setContracts] = useState<Contract[]>([]);
+  const [contractsPage, setContractsPage] = useState(1);
+  const [contractsAll, setContractsAll] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
+  useEffect(() => {
+    spaceTraderClient.ContractsClient.getContracts(
+      contractsPage,
+      itemsPerPage
+    ).then((response) => {
+      console.log("response", response);
+      setContracts(response.data.data);
+      setContractsAll(response.data.meta.total);
+    });
+    return () => {};
+  }, [contractsPage, itemsPerPage]);
+
+  const onChange: PaginationProps["onChange"] = (page, pageSize) => {
+    console.log(page);
+    setContractsPage(page);
+    setItemsPerPage(pageSize);
+  };
+
+  return (
+    <div>
+      <h2>All Factions</h2>
+      <Pagination
+        current={contractsPage}
+        onChange={onChange}
+        total={contractsAll}
+        pageSizeOptions={[5, 10, 15, 20]}
+        showTotal={(total, range) =>
+          `${range[0]}-${range[1]} of ${total} items`
+        }
+        style={{ padding: "16px", textAlign: "center" }}
+      />
+      <Flex wrap gap="middle" align="center" justify="space-evenly">
+        {contracts.map((value) => {
+          return (
+            <Card key={value.id} style={{ width: "fit-content" }}>
+              <Descriptions
+                bordered
+                layout="vertical"
+                items={[
+                  {
+                    label: "Id",
+                    key: "id",
+                    children: value.id,
+                  },
+                  {
+                    label: "accepted",
+                    key: "accepted",
+                    children: value.accepted,
+                  },
+                  {
+                    label: "deadlineToAccept",
+                    key: "deadlineToAccept",
+                    children: value.deadlineToAccept,
+                  },
+                  {
+                    label: "factionSymbol",
+                    key: "factionSymbol",
+                    children: value.factionSymbol,
+                  },
+                  {
+                    label: "fulfilled",
+                    key: "fulfilled",
+                    children: value.fulfilled,
+                  },
+                  {
+                    label: "terms.deadline",
+                    key: "terms.deadline",
+                    children: value.terms.deadline,
+                  },
+                  {
+                    label: "terms.payment.onAccepted",
+                    key: "terms.payment.onAccepted",
+                    children: value.terms.payment.onAccepted,
+                  },
+                  {
+                    label: "terms.payment.onFulfilled",
+                    key: "terms.payment.onFulfilled",
+                    children: value.terms.payment.onFulfilled,
+                  },
+                  {
+                    label: "terms.deliver",
+                    key: "terms.deliver",
+                    children: value.terms.deliver?.join(" - "),
+                  },
+                  {
+                    label: "type",
+                    key: "type",
+                    children: value.type,
+                  },
+                  {
+                    label: "expiration",
+                    key: "expiration",
+                    children: value.expiration,
+                  },
+                ]}
+              ></Descriptions>
+            </Card>
+          );
+        })}
+      </Flex>
+    </div>
+  );
 }
 
 export default Contracts;
--- src/sites/factions/Factions.tsx
+++ src/sites/factions/Factions.tsx
@@ -1,5 +1,111 @@
+import {
+  PaginationProps,
+  Pagination,
+  Flex,
+  Card,
+  Descriptions,
+  Tooltip,
+  List,
+} from "antd";
+import { useState, useEffect } from "react";
+import { Faction } from "../../components/api";
+import spaceTraderClient from "../../spaceTraderClient";
+
 function Factions() {
-  return <div>Factions</div>;
+  const [factions, setFactions] = useState<Faction[]>([]);
+  const [factionsPage, setFactionsPage] = useState(1);
+  const [factionsAll, setFactionsAll] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
+  useEffect(() => {
+    spaceTraderClient.FactionsClient.getFactions(
+      factionsPage,
+      itemsPerPage
+    ).then((response) => {
+      console.log("response", response);
+      setFactions(response.data.data);
+      setFactionsAll(response.data.meta.total);
+    });
+    return () => {};
+  }, [factionsPage, itemsPerPage]);
+
+  const onChange: PaginationProps["onChange"] = (page, pageSize) => {
+    console.log(page);
+    setFactionsPage(page);
+    setItemsPerPage(pageSize);
+  };
+
+  return (
+    <div>
+      <h2>All Factions</h2>
+      <Pagination
+        current={factionsPage}
+        onChange={onChange}
+        total={factionsAll}
+        pageSizeOptions={[5, 10, 15, 20]}
+        showTotal={(total, range) =>
+          `${range[0]}-${range[1]} of ${total} items`
+        }
+        style={{ padding: "16px", textAlign: "center" }}
+      />
+      <Flex wrap gap="middle" align="center" justify="space-evenly">
+        {factions.map((value) => {
+          return (
+            <Card key={value.symbol} style={{ width: "fit-content" }}>
+              <Descriptions
+                bordered
+                layout="vertical"
+                items={[
+                  {
+                    label: "Symbol",
+                    key: "symbol",
+                    children: value.symbol,
+                  },
+                  {
+                    label: "Name",
+                    key: "name",
+                    children: value.name,
+                  },
+                  {
+                    label: "Description",
+                    key: "description",
+                    children: value.description,
+                  },
+                  {
+                    label: "Headquarters",
+                    key: "headquarters",
+                    children: value.headquarters,
+                  },
+                  {
+                    label: "IsRecruiting",
+                    key: "isRecruiting",
+                    children: value.isRecruiting ? "Yes" : "No",
+                  },
+                  {
+                    label: "Traits",
+                    key: "traits",
+                    children: (
+                      <List
+                        size="small"
+                        dataSource={value.traits.map((trait) => (
+                          <Tooltip
+                            key={trait.symbol}
+                            title={`${trait.symbol} - ${trait.description}`}
+                          >
+                            <span>{trait.name}</span>
+                          </Tooltip>
+                        ))}
+                        renderItem={(item) => <List.Item>{item}</List.Item>}
+                      ></List>
+                    ),
+                  },
+                ]}
+              ></Descriptions>
+            </Card>
+          );
+        })}
+      </Flex>
+    </div>
+  );
 }
 
 export default Factions;
